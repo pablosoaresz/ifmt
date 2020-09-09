f20df39b4f2864808a59ff34d66fb2cfeb61b4d8
pabloetf@gmail.com <pabloetf@gmail.com>
2020-09-08 21:25:59 -0400

    Implementando o Controller

---
+++ cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/model/StatusTitulo.java
@@ -0,0 +1,16 @@
+package edu.ifmt.cobrancaifmt.model;
+
+public enum StatusTitulo {
+
+	PENDENTE("Pendente"), RECEBIDO("Recebido");
+
+	private String descricao;
+
+	StatusTitulo(String descricao) {
+		this.descricao = descricao;
+
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+}
