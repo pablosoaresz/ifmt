e584a6929a5e99c6f163ab3dd55491b6fbfa3685
pabloetf@gmail.com <pabloetf@gmail.com>
2020-09-09 22:04:02 -0400

    erro.jpg

---
+++ cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/repository/Titulos.java
@@ -0,0 +1,9 @@
+package edu.ifmt.cobrancaifmt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ifmt.cobrancaifmt.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
