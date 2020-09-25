7facc8ef05b9b84fc58ecd27d57983cd42e9d996
pabloetf@gmail.com <pabloetf@gmail.com>
2020-09-25 19:55:26 -0400

    Apostila 3 finalizada. Sistema funcionando conforme apostila

--- cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
+++ cobrancaifmt/src/main/java/edu/ifmt/cobrancaifmt/controller/TituloController.java
@@ -2,7 +2,6 @@ package edu.ifmt.cobrancaifmt.controller;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import edu.ifmt.cobrancaifmt.model.StatusTitulo;
 import edu.ifmt.cobrancaifmt.model.Titulo;
 import edu.ifmt.cobrancaifmt.repository.Titulos;
